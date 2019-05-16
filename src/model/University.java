858bfb7677686be4c96b0c4f3fd6047372d86af9
Mateo Loaiza Zuniga <1006106049@academico.icesi.edu.co>
2019-05-16 18:10:47 -0500

    I added the pre order method and the main just for the activity in the class

--- src/model/University.java
+++ src/model/University.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class University {
 
 	private String name;
@@ -49,6 +52,36 @@ public class University {
 		}
 
 	}
+	
+	   public List<AcademicUnit> preorder(){
+	        return preorder(root);  
+	    }
+	    private List<AcademicUnit> preorder(AcademicUnit current){
+	        List<AcademicUnit> lis= new ArrayList<>();
+	        if(current != null) {
+	            lis.add(current);
+	            List<AcademicUnit> lis2 = preorder(current.getLeft());
+	            List<AcademicUnit> lis3 = preorder(current.getRight());
+	            lis.addAll(lis2);
+	            lis.addAll(lis3);
+	        }
+	        return lis;
+	    }
+	    
+	    //Mejor usar este
+	    public List<AcademicUnit> preorder2(){
+	        List<AcademicUnit> lis= new ArrayList<>();
+	        preorder2(root,lis);
+	        return lis;
+	    }
+	    private void preorder2(AcademicUnit current,List<AcademicUnit> lis){
+	        if(current != null) {
+	            lis.add(current);
+	            preorder2(current.getLeft(),lis);
+	            preorder2(current.getRight(),lis);
+	            
+	        }
+	    }
 
 
 }
---
+++ src/userInterface/Main.java
@@ -0,0 +1,9 @@
+package userInterface;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println((4+7)%22);
+	}
+	
+}
